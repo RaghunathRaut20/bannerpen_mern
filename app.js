09c9e4c937465da263b2152c5a1bf48e25a82230
RaghunathRaut <raghunathraut20@gmail.com>
2021-07-20 18:24:18 +0530

    submit

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ require('./db/conn');
 require("./model/userSchema");
 
 const DB = process.env.DATABASE;
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(require('./router/auth'));
+
+
+if(process.env.NODE_ENV = "production"){
+    app.use(express.static("client/build"));
+}
 
 app.listen(port, (err) => {
     console.log(`APP is running onq  ${port}`)
